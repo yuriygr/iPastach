cb02cbfd543b004407694884048f9bf8cb11df48
Юрий Гринёв <a1d516ac5f5d290@gmail.com>
2018-05-28 17:25:16 +0500

    Немного изменений API

--- iPastach/ApiManager.swift
+++ iPastach/ApiManager.swift
@@ -1,5 +1,5 @@
 //
-//  ApiManager.swift
+//  APIManager.swift
 //  iPastach
 //
 //  Created by Юрий Гринев on 11.03.2018.
@@ -8,17 +8,39 @@
 
 import UIKit
 
-class ApiManager: NSObject {
+class APIManager: NSObject {
       
-    static let shared: ApiManager = ApiManager()
+    static let shared: APIManager = APIManager()
 
     // Ещё немного чего-то крутого и потрясного
     private let URLSession: URLSession = .shared
     
     /// Base path to API
-    private let path: String = "https://api.pastach.ru/"
+    private let APIBase: String = "https://api.pastach.ru/"
     
-    ///
+    // Временное
+    enum APIEndpoints {
+        enum pastes: String {
+            case list = "pastes.list"
+            case item = "pastes.item"
+        }
+        enum tags: String {
+            case list = "tags.list"
+        }
+    }
+    
+    // Алиас для параметров
+    typealias APIParams = [String: String]
+    
+    /// HTTP Methods
+    enum HTTPMethods: String {
+        case POST
+        case GET
+        case PUT
+        case DELETE
+    }
+
+    /// Errors
     /// - invalidURL
     /// - invalidParams
     /// - undefined
@@ -36,16 +58,15 @@ class ApiManager: NSObject {
     /// - Parameter completion: Кложур для работы с данными
     ///
     /// - Returns: Nothing to return
-    private func fetch<T>(_ type: T.Type, endpoint: String, params: [String:String] = [:], completion: @escaping (T?, Error?) -> Void) where T: Decodable {
+    private func fetch<T>(_ type: T.Type, endpoint: String, params: APIParams = [:], method: HTTPMethods = .POST, completion: @escaping (T?, Error?) -> Void) where T: Decodable {
 
-        guard let url = URL(string: self.path + endpoint) else {
+        guard let url = URL(string: APIBase + endpoint) else {
             return completion(nil, Errors.invalidURL)
         }
 
         var request = URLRequest(url: url)
-        request.httpMethod = "POST"
+        request.httpMethod = method.rawValue
         request.setBodyContent(params)
-        request.setValue("x-session", forHTTPHeaderField: "Sup")
 
         URLSession.dataTask(with: request) { (data, response, error) in
             if error != nil {
@@ -63,64 +84,11 @@ class ApiManager: NSObject {
     }
 
     //MARK: - Временные постоянные решения
-    enum pastesMethod: String {
-        case list
-        case item
+    func pastes<T>(_ type: T.Type, endpoint: APIEndpoints.pastes, params: APIParams = [:], completion: @escaping (T?, Error?) -> Void) where T: Decodable {
+        fetch(type, endpoint: endpoint.rawValue, params: params, completion: completion)
     }
-    func pastes<T>(_ type: T.Type, method: pastesMethod, params: [String:String] = [:], completion: @escaping (T?, Error?) -> Void) where T: Decodable {
-        let endproint = "pastes.\(method.rawValue)"
-        self.fetch(type, endpoint: endproint, params: params, completion: completion)
-    }
-    
-    enum tagsMethod: String {
-        case list
-    }
-    func tags<T>(_ type: T.Type, method: tagsMethod, params: [String:String] = [:], completion: @escaping (T?, Error?) -> Void) where T: Decodable {
-        let endproint = "tags.\(method.rawValue)"
-        self.fetch(type, endpoint: endproint, params: params, completion: completion)
-    }
-}
 
-extension URLRequest {
-    
-    /// Populate the HTTPBody of `application/x-www-form-urlencoded` request
-    ///
-    /// - Parameter parameters:   A dictionary of keys and values to be added to the request
-    mutating func setBodyContent(_ parameters: [String : String]) {
-        let parameterArray = parameters.map { (key, value) -> String in
-            let encodedKey   = key.addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed)!
-            let encodedValue = value.addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed)!
-            return "\(encodedKey)=\(encodedValue)"
-        }
-        httpBody = parameterArray
-            .joined(separator: "&")
-            .data(using: .utf8)
+    func tags<T>(_ type: T.Type, endpoint: APIEndpoints.tags, params: APIParams = [:], completion: @escaping (T?, Error?) -> Void) where T: Decodable {
+        fetch(type, endpoint: endpoint.rawValue, params: params, completion: completion)
     }
-}
-
-extension CharacterSet {
-    
-    /// Character set containing characters allowed in query value as outlined in RFC 3986.
-    ///
-    /// RFC 3986 states that the following characters are "reserved" characters.
-    ///
-    /// - General Delimiters: ":", "#", "[", "]", "@", "?", "/"
-    /// - Sub-Delimiters: "!", "$", "&", "'", "(", ")", "*", "+", ",", ";", "="
-    ///
-    /// In RFC 3986 - Section 3.4, it states that the "?" and "/" characters should not be escaped to allow
-    /// query strings to include a URL. Therefore, all "reserved" characters with the exception of "?" and "/"
-    /// should be percent-escaped in the query string.
-    ///
-    /// - Parameter string: The string to be percent-escaped.
-    ///
-    /// - Returns: The percent-escaped string.
-    static let urlQueryValueAllowed: CharacterSet = {
-        let generalDelimitersToEncode = ":#[]@" // does not include "?" or "/" due to RFC 3986 - Section 3.4
-        let subDelimitersToEncode = "!$&'()*+,;="
-        
-        var allowed = CharacterSet.urlQueryAllowed
-        allowed.remove(charactersIn: generalDelimitersToEncode + subDelimitersToEncode)
-        
-        return allowed
-    }()
 }
